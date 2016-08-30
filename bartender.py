1c4850a4f66e54ff1e0028f75eabc4c65fa2a97b
holler4amarshall <holler4amarshall@hotmail.com>
2016-08-30 18:17:06 +1000

    added dictionary & function to generate random names for the cocktails

--- bartender.py
+++ bartender.py
@@ -6,7 +6,7 @@ questions = {
         "bitter": "Are ye a lubber who likes it bitter?",
         "sweet": "Would ye like a bit of sweetness with yer poison?",
         "fruity": "Are ye one for a fruity finish?",
-    }
+        }
 
 ingredients = {
         "strong": ["glug of rum", "slug of whisky", "splash of gin"],
@@ -14,7 +14,12 @@ ingredients = {
         "bitter": ["shake of bitters", "splash of tonic", "twist of lemon peel"],
         "sweet": ["sugar cube", "spoonful of honey", "spash of cola"],
         "fruity": ["slice of orange", "dash of cassis", "cherry on top"],
-    }
+        }
+    
+names = {
+        "descriptives": ["jumping", "whirling", "fluffy", "cranky", "porky"], 
+        "nouns": ["lion", "bunny", "ship", "pirate", "holly", "dolphin"],
+        }
 
 responses = {}
 
@@ -45,6 +50,16 @@ def mixed_drink(preferences):
 
 #mixed_drink(preferences)
 
+def cocktail_name():
+        '''a function to return a cocktail name based on a randomly selected descriptive word and a noun'''
+        name = ""
+        for adjective, noun in names.items():
+                first_name = names['descriptives']
+                last_name = names['nouns']
+                name = random.choice(first_name) + ' ' + random.choice(last_name)
+     
+        return name
+
 def main():
         '''a function to run mixed_drink function, with arguments from preferences \
         function to return a list of ingredients that will be included in the drink. \
@@ -54,11 +69,8 @@ def main():
         suggested_drink_sentence = ""
         for item in suggested_drink: 
                 suggested_drink_sentence += item + ", "
-        print "...Nice choice, Captain. 1 special drink with a " + suggested_drink_sentence \
+        print "...Nice choice, Captain. 1 " + cocktail_name() + " with a " + suggested_drink_sentence \
         + "poured over ice and specially mixed by your favourite Pirate, coming right up!"
 
 if __name__ == '__main__':
     main()
-
-
-
